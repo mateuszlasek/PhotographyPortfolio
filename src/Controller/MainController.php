671aec1f59540fcbfe7d518af43a16b26cc4de44
mateuszlasek <mateusz.lasek17@wp.pl>
2024-06-19 19:30:33 +0200

    add cache to photos

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,16 +3,28 @@
 namespace App\Controller;
 
 use App\Repository\PhotoRepository;
+use Psr\Cache\InvalidArgumentException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Contracts\Cache\CacheInterface;
+use Symfony\Contracts\Cache\ItemInterface;
 
 class MainController extends AbstractController
 {
+    /**
+     * @throws InvalidArgumentException
+     */
     #[Route('/main', name: 'app_main')]
-    public function index(PhotoRepository $photoRepository): Response
+    public function index(PhotoRepository $photoRepository, CacheInterface $cache): Response
     {
-        $photos = $photoRepository->findAll();
+        $cacheKey = 'main_index_photos';
+
+        $photos = $cache->get($cacheKey, function (ItemInterface $item) use ($photoRepository) {
+            $item->expiresAfter(3600);
+
+            return $photoRepository->findAll();
+        });
 
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
