6d898f05edafd89d276875bc9c19e53ca0afb27e
mateuszlasek <mateusz.lasek17@wp.pl>
2024-06-19 18:47:46 +0200

    fetch all photos from database and parse to twig

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 class MainController extends AbstractController
 {
     #[Route('/main', name: 'app_main')]
-    public function index(): Response
+    public function index(PhotoRepository $photoRepository): Response
     {
+        $photos = $photoRepository->findAll();
+
         return $this->render('main/index.html.twig', [
             'controller_name' => 'MainController',
+            'photos' => $photos
         ]);
     }
 }
